8db5846d282d547fb4a959151af3ca504a32fc20
Michael Joyner <joyner.michael@gmail.com>
2016-01-17 17:06:56 +0800

    add social links in footer

--- resources/views/front/partials/footer.blade.php
+++ resources/views/front/partials/footer.blade.php
@@ -3,9 +3,16 @@
     <div class="footer-col">
         <h3>Be Social</h3>
         <div class="social-links">
-            <a class="social-link-icon" href="#"><img src="{{ asset('images/assets/twitter.png') }}" alt="twitter link"></a>
-            <a class="social-link-icon" href="#"><img src="{{ asset('images/assets/fb.png') }}" alt="twitter link"></a>
-            <a class="social-link-icon" href="#"><img src="{{ asset('images/assets/email.png') }}" alt="twitter link"></a>
+            <a class="social-link-icon" href="https://twitter.com/2CompleteLiving">
+                <img src="{{ asset('images/assets/twitter.png') }}" alt="twitter link">
+            </a>
+            <a class="social-link-icon" href="https://www.facebook.com/CompleteLiving">
+                <img src="{{ asset('images/assets/fb.png') }}" alt="twitter link">
+            </a>
+            <a class="social-link-icon"
+               href="ma&#105;l&#116;o&#58;&#115;t&#101;&#112;h&#106;&#111;y&#110;&#101;&#114;1&#56;&#64;&#103;mail&#46;&#99;&#111;&#109;">
+                <img src="{{ asset('images/assets/email.png') }}" alt="twitter link">
+            </a>
         </div>
     </div>
     <div class="footer-col">
